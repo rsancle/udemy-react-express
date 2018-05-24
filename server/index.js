5c0c842735a214a811e8236504ce8208a46d01fe
rsancle <raulsancal@gmail.com>
2018-05-24 15:16:12 +0200

    added passport js with google integration

--- server/index.js
+++ server/index.js
@@ -1,44 +1,43 @@
 const express = require('express');
-const mongoose = require('mongoose');
-const cookieSession = require('cookie-session');
 const passport = require('passport');
-const bodyParser = require('body-parser');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const keys = require('./config/keys');
-require('./models/User');
-require('./models/Survey');
-require('./services/passport');
 
-mongoose.Promise = global.Promise;
-mongoose.connect(keys.mongoURI);
-
+//express() generates new express app
 const app = express();
 
-app.use(bodyParser.json());
-app.use(
-  cookieSession({
-    maxAge: 30 * 24 * 60 * 60 * 1000,
-    keys: [keys.cookieKey]
-  })
+
+//Set what strategy we will use
+passport.use(
+    new GoogleStrategy(
+        {
+            clientID: keys.googleClientID,
+            clientSecret: keys.googleClientSecret,
+            callbackURL: '/auth/google/callback'
+        },
+        (accessToken, refreshToken, profile, done) => {
+            console.log(accessToken);
+        }
+    )
 );
-app.use(passport.initialize());
-app.use(passport.session());
 
-require('./routes/authRoutes')(app);
-require('./routes/billingRoutes')(app);
-require('./routes/surveyRoutes')(app);
+//route handler
+/*app.get('/', (req, res) => {
+    res.send({ hi: 'there' });
+});*/
 
-if (process.env.NODE_ENV === 'production') {
-  // Express will serve up production assets
-  // like our main.js file, or main.css file!
-  app.use(express.static('client/build'));
+app.get(
+    '/auth/google',
+    passport.authenticate(
+        'google',
+        {
+            scope: ['profile', 'email']
+        }
+    )
+);
 
-  // Express will serve up the index.html file
-  // if it doesn't recognize the route
-  const path = require('path');
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
-}
+app.get('/auth/googlecallback', passport.authenticate('google'));
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
