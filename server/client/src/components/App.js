68589dc4c5b84844d71bbc9473f441b516a203be
rsancle <raulsancal@gmail.com>
2018-05-30 17:30:05 +0200

    added routes with some dummies components to test it

--- server/client/src/components/App.js
+++ server/client/src/components/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+const Header = () => <h2>Header</h2>;
+const Dashboard = () => <h2>Dashboard</h2>;
+const SurveyNew = () => <h2>SurveyNew</h2>;
+const Landing = () => <h2>Landing</h2>;
 
 const App = () => {
     return(
-        <p>
-            Hi there
-        </p>
+        <div>
+            <BrowserRouter>
+                <div>
+                    <Route path="/" component={ Landing } />
+                </div>
+            </BrowserRouter>
+        </div>
     );
 }
 
